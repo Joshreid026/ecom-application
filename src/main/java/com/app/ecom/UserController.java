8b969fffc67b7b21fb0ab2f02562214e02185412
Josh Reid <Reid-J55@ulster.ac.uk>
2026-08-23 18:01:31 +0100

    Initial commit with controller and service layer

---
+++ src/main/java/com/app/ecom/UserController.java
@@ -0,0 +1,41 @@
+package com.app.ecom;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/api/users")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/api/users/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id){
+        return userService.fetchUser(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/api/users")
+    public ResponseEntity<String> createUser(@RequestBody User user){
+        userService.addUser(user);
+        return new ResponseEntity<>("User added successfully", HttpStatus.OK);
+    }
+
+    @PutMapping("/api/users/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody User updatedUser){
+        boolean updated = userService.updateUser(id, updatedUser);
+        if(updated)
+            return ResponseEntity.ok("User updated successfully");
+        return ResponseEntity.notFound().build();
+    }
+}
