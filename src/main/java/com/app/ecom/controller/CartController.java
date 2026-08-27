92999c777420dedc5e3f6f85ac24bf9df2cfb8d6
Josh Reid <Reid-J55@ulster.ac.uk>
2026-08-27 15:32:20 +0100

    view cart endpoint

--- src/main/java/com/app/ecom/controller/CartController.java
+++ src/main/java/com/app/ecom/controller/CartController.java
@@ -1,12 +1,14 @@
 package com.app.ecom.controller;
 
 import com.app.ecom.dto.CartItemRequest;
+import com.app.ecom.model.CartItem;
 import com.app.ecom.service.CartService;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cart")
@@ -32,5 +34,11 @@ public class CartController {
         boolean deleted = cartService.deleteItemFromCart(userId, productId);
         return deleted ? ResponseEntity.noContent().build()
                 : ResponseEntity.notFound().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CartItem>> getCart(
+            @RequestHeader("X-USER-ID") String userId) {
+        return ResponseEntity.ok(cartService.getCart(userId));
     }
 }
