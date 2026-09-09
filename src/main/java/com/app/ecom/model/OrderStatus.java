4c031cd229f2c77b44c79ec4f785ba95aa6d545d
joreid-r7 <josh_reid@rapid7.com>
2026-09-09 16:57:53 +0100

    removed noise

--- src/main/java/com/app/ecom/model/OrderStatus.java
+++ src/main/java/com/app/ecom/model/OrderStatus.java
@@ -1,8 +1,3 @@
-/***************************************************************************
- * COPYRIGHT (C) 2012-2026, Rapid7 LLC, Boston, MA, USA.
- * All rights reserved. This material contains unpublished, copyrighted
- * work including confidential and proprietary information of Rapid7.
- **************************************************************************/
 package com.app.ecom.model;
 
 public enum OrderStatus {
