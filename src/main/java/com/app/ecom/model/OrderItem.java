94b336cbf7c2132e4b7eba0c8666d9c87e6a5138
joreid-r7 <josh_reid@rapid7.com>
2026-09-10 16:50:10 +0100

    added order features

--- src/main/java/com/app/ecom/model/OrderItem.java
+++ src/main/java/com/app/ecom/model/OrderItem.java
@@ -8,11 +8,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class OrderItem {
     @Id
